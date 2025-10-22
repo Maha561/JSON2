cc5fb60facb3d126c66810325bb2aaca1f4909d2
Maha561 <mahalakshmianandan04@gmail.com>
2025-10-22 16:13:45 +0530

    added

---
+++ myTutorials.js
@@ -0,0 +1,9 @@
+
+
+
+var menuItems = [
+    { "url": "https://google.com", "display": "Google" },
+    { "url": "https://facebook.com", "display": "Facebook" },
+    { "url": "https://twitter.com", "display": "Twitter" }
+];
+
